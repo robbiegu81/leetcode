1966c4536be6ddaae334861201f02d317d16e178
gu <99523319@qq.com>
2019-03-21 19:47:40 +0800

    1.两数之和

---
+++ easy/src/S0001.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ *
+ * https://leetcode-cn.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (45.23%)
+ * Total Accepted:    279.2K
+ * Total Submissions: 617.3K
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ *
+ */
+public class S0001 {
+
+    // public int[] twoSum(int[] nums, int target) {
+
+    //     for (int i = 0; i < nums.length; i++) {
+    //         int first = nums[i];
+    //         int second = target - first;
+
+    //         for (int j = i + 1; j < nums.length; j++) {
+    //             if(nums[j] == second ) {
+    //                 return new int[]{i, j};
+    //             }
+    //         }
+    //     }
+    //     throw new RuntimeException();
+
+    // }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int second = target - nums[i];
+            if(map.containsKey(new Integer(second))){
+                return new int[]{map.get(new Integer(second)).intValue(), i};
+            }
+            map.put(new Integer(nums[i]), i);
+        }
+        throw new RuntimeException();
+
+    }
+
+    public static void main(String[] args) {
+        S0001 sol = new S0001();
+        int[] nums = {0, 4, 3, 0};
+        System.out.println(sol.twoSum(nums, 0)[1]);
+    }
+}
